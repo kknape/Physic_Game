49b00d2fef6595e444bf5f1809664010732ba07e
k2k <kadonna@wonderkul.com>
2019-06-09 22:03:41 -0500

    making progress. stuck on getting my Wins counter to increment. and the replay and reset parts.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -2,9 +2,11 @@
 // -----START OF GAME-----
 //start of game, the Page displays values of 0 for Wins, Losses and 9 for Attempts
 var wins = 0;
+ document.getElementById ("wins").innerHTML = ("Wins: " + wins);
 var losses = 0;
+    document.getElementById ("losses").innerHTML = ("Losses: " + losses);
 var attemptsLeft = 9;
-    document.getElementById ("attemptsLeft").textContent = "Guesses left: " + attemptsLeft;
+    document.getElementById ("attemptsLeft").innerHTML = ("Attempts left: " + attemptsLeft);
 var guessSoFar = [];
 var guess = 0;
 var guessedLetter = "";
@@ -27,7 +29,6 @@ function userGuess(event) {
         //check if guess is a letter
              if(guess >= 65 && guess <= 90) {
               var guess = String.fromCharCode(guess);
-       //     document.getElementById("guessSoFar").innerHTML = "The pressed key was: " + guess;
                 }
 
                  //if the guess isn't a letter, ask them to choose a letter.
@@ -50,24 +51,27 @@ function userGuess(event) {
         //if quess does not equal the random letter, subtract 1 from attempts left, add the guessed letter to Guesses so far
         //user guesses again
             else {
-                if (attemptsLeft = 0)
+                if (attemptsLeft <= 0)
                 { 
-                    losses++;
-                    document.getElementById ("losses").innerHTML = " " + losses;
+                     document.getElementById("losses").innerHTML = ("Losses: " + (losses++));
 
        //             guessSoFar.push(guess);
       //              document.getElementById("guessSoFar").innerHTML = "  " + guessSoFar.join(",");
 
                 }
                 else {
-                    attemptsLeft = (attemptsLeft - 1);
-                    document.getElementById ("attemptsLeft").innerHTML = " " + attemptsLeft;
-
-                    userGuess;
+                    alert ("Sorry, that's not it. Guess again.");
+                    document.getElementById("attemptsLeft").innerHTML = ("Attempts left: " + (attemptsLeft - 1));
+                    guessSoFar.push(guess);
+                    document.getElementById("guessSoFar").innerHTML = "Your guesses so far: " + guessSoFar.join(",");
+                
+                }   
+                    
+                    }   
                 }
-            }    
-    }
+                
 
- document.onkeyup = function(event){
-    userGuess(event);
-}   
+    document.onkeyup = function(event){
+            userGuess(event);
+                }   
+    
